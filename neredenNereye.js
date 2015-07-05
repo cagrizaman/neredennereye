a8bc7c9a2a1dc5526b1bc461c12eed5bb5f7e9a4
cagrizaman <zaman@mit.edu>
2015-07-05 18:11:22 -0400

    leaflet map added

--- neredenNereye.js
+++ neredenNereye.js
@@ -1,7 +1,19 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  L.Icon.Default.imagePath = 'packages/fuatsengul_leaflet/1.0.1/web.browser/packages/fuatsengul\:leaflet/images/';
+  
+  Meteor.startup(function(){
+    var map = L.map('map').setView([51.505, -0.09], 13);
 
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+  })
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
