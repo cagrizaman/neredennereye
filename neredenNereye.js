d99b0ef08536e704089284c4ddd99912b7ec30e1
cagrizaman <zaman@mit.edu>
2015-07-22 03:08:10 -0400

    Google Maps destegi eklendi

--- neredenNereye.js
+++ neredenNereye.js
@@ -8,58 +8,73 @@ if (Meteor.isClient) {
         return jQuery(this)
     }
 })
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-  L.Icon.Default.imagePath = 'packages/fuatsengul_leaflet/1.0.1/web.browser/packages/fuatsengul\:leaflet/images/';
+
+//   L.Icon.Default.imagePath = 'packages/fuatsengul_leaflet/1.0.1/web.browser/packages/fuatsengul\:leaflet/images/';
   
   Meteor.startup(function(){
+  
+  GoogleMaps.load({v:'3',libraries:'geometry,places'});
 
-    var map = L.map('map').setView([41.015137, 28.979530], 10);
+//OLD LEAFLET MAP
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+//     var map = L.map('map').setView([41.015137, 28.979530], 10);
+
+//     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//     }).addTo(map);
 
   
 
-var neredenIcon = L.icon({
-    iconUrl: 'neredenmarker.png',
+// var neredenIcon = L.icon({
+//     iconUrl: 'neredenmarker.png',
 
-    iconSize:     [60, 50], 
-    iconAnchor:   [14, 50],
-});
+//     iconSize:     [60, 50], 
+//     iconAnchor:   [14, 50],
+// });
 
-  L.marker([41.015137, 28.979530], {icon: neredenIcon}).addTo(map);
+//   L.marker([41.015137, 28.979530], {icon: neredenIcon}).addTo(map);
 
 
-  var nereyeIcon = L.icon({
-    iconUrl: 'nereyemarker.png',
+//   var nereyeIcon = L.icon({
+//     iconUrl: 'nereyemarker.png',
 
-    iconSize:     [60, 50], 
-    iconAnchor:   [14, 50],
-});
+//     iconSize:     [60, 50], 
+//     iconAnchor:   [14, 50],
+// });
 
-  L.marker([41.125137, 29.979530], {icon: nereyeIcon}).addTo(map);
+//   L.marker([41.125137, 29.979530], {icon: nereyeIcon}).addTo(map);
     
 
-var pointA = new L.LatLng(41.015137, 28.979530);
-var pointB = new L.LatLng(41.125137, 29.979530);
-var pointList = [pointA, pointB];
+// var pointA = new L.LatLng(41.015137, 28.979530);
+// var pointB = new L.LatLng(41.125137, 29.979530);
+// var pointList = [pointA, pointB];
 
-var Polyline= new L.Polyline(pointList, {
-color: '#D49A6A',
-weight: 6,
-opacity: 0.9,
-smoothFactor: 1
+// var Polyline= new L.Polyline(pointList, {
+// color: '#D49A6A',
+// weight: 6,
+// opacity: 0.9,
+// smoothFactor: 1
+// });
+// Polyline.addTo(map);
+// map.fitBounds(Polyline.getBounds());
+
+     
+
+      });
+
+
+Template.map.helpers({  
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(41.0, 28.9631),
+        zoom:12
+      };
+    }
+  }
 });
-Polyline.addTo(map);
 
-    map.fitBounds(Polyline.getBounds());
-
-      })
-
-
-  Template.template_survey.rendered=function(){
+Template.template_survey.rendered=function(){
     $("#input_date").datepicker( {
     format: "mm-yyyy",
     startView: "months", 
@@ -67,6 +82,35 @@ Polyline.addTo(map);
 });
 
   };
+
+
+  Template.map.onCreated(function() {  
+  GoogleMaps.ready('map', function(map) {
+     console.log("I'm ready!");
+       var neredenCustom = {
+                    url:'neredenmarker.png',
+                    size: new google.maps.Size(60,50),
+                    origin: new google.maps.Point(0,0),
+                    anchor: new google.maps.Point(14,50)
+    };
+     var marker = new google.maps.Marker({
+              position: new google.maps.LatLng(41.000,29.0),
+              map: map.instance,
+              icon: neredenCustom
+        });
+  
+    var defaultBounds = map.instance.getBounds();
+
+    var input = document.getElementById('nereden');
+    var options = {
+      bounds: defaultBounds,
+      types: ['geocode']
+};
+
+autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  });
+  });
 }
 
 if (Meteor.isServer) {
